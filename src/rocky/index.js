460cda235908f641e649671302cf10683155f627
Edward Dam <edward.dam@gmail.com>
2017-01-01 19:38:50 +0000

    Updated with new watchface look

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -53,33 +53,31 @@ rocky.on('draw', function(event) {
   var cy = height / 2;
   
   // Draw Fixed Text
-  drawText(ctx, 'pebble',    'white', 'center', '24px bold Gothic', cx, cy - 57);
-  drawText(ctx, 'edwarddam', 'white', 'center', '14px bold Gothic', cx, cy - 32);
+  drawText(ctx, 'pebble', 'white', 'center', '24px bold Gothic', cx, cy - 55);
+  drawText(ctx, 'Ed',     'gray',  'center', '14px bold Gothic', 35, cy - 15);
+  drawText(ctx, 'Dam',    'gray',  'center', '14px bold Gothic', 35, cy - 5);
   
   // Draw Numbers
-  drawText(ctx, '1',  'white', 'center', '20px bold Leco-numbers', cx + 40, 0);
-  drawText(ctx, '2',  'white', 'right',  '20px bold Leco-numbers', width,   cy     - 50);
-  drawText(ctx, '3',  'white', 'right',  '20px bold Leco-numbers', width,   cy     - 13);
-  drawText(ctx, '4',  'white', 'right',  '20px bold Leco-numbers', width,   cy     + 25);
-  drawText(ctx, '5',  'white', 'center', '20px bold Leco-numbers', cx + 40, height - 25);
-  drawText(ctx, '6',  'white', 'center', '20px bold Leco-numbers', cx,      height - 25);
-  drawText(ctx, '7',  'white', 'center', '20px bold Leco-numbers', cx - 40, height - 25);
-  drawText(ctx, '8',  'white', 'left',   '20px bold Leco-numbers', 0,       cy     + 25);
-  drawText(ctx, '9',  'white', 'left',   '20px bold Leco-numbers', 0,       cy     - 13);
-  drawText(ctx, '10', 'white', 'left',   '20px bold Leco-numbers', 0,       cy     - 50);
-  drawText(ctx, '11', 'white', 'center', '20px bold Leco-numbers', cx - 40, 0);
-  drawText(ctx, '12', 'white', 'center', '20px bold Leco-numbers', cx,      0);
+  drawText(ctx, '1',  'white', 'center', '28px bold Droid-serif', cx + 40, - 6);
+  drawText(ctx, '2',  'white', 'right',  '28px bold Droid-serif', width,   cy     - 55);
+  drawText(ctx, '3',  'white', 'right',  '28px bold Droid-serif', width,   cy     - 18);
+  drawText(ctx, '4',  'white', 'right',  '28px bold Droid-serif', width,   cy     + 20);
+  drawText(ctx, '5',  'white', 'center', '28px bold Droid-serif', cx + 40, height - 29);
+  drawText(ctx, '6',  'white', 'center', '28px bold Droid-serif', cx,      height - 29);
+  drawText(ctx, '7',  'white', 'center', '28px bold Droid-serif', cx - 40, height - 29);
+  drawText(ctx, '8',  'white', 'left',   '28px bold Droid-serif', 0,       cy     + 20);
+  drawText(ctx, '9',  'white', 'left',   '28px bold Droid-serif', 0,       cy     - 18);
+  drawText(ctx, '10', 'white', 'left',   '28px bold Droid-serif', 0,       cy     - 55);
+  drawText(ctx, '11', 'white', 'center', '28px bold Droid-serif', cx - 40, - 6);
+  drawText(ctx, '12', 'white', 'center', '28px bold Droid-serif', cx,      - 6);
   
   // Draw Day
-  var dayNames  = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  var dayDate   = dayNames[new Date().getDay()];
-  drawText(ctx, dayDate, 'white', 'center', '14px bold Gothic', cx, cy + 15);
+  var day = new Date().toLocaleDateString(undefined, {day: 'short'});
+  drawText(ctx, day, 'gray', 'center', '14px bold Gothic', width - 33, cy - 15);
   
   // Draw Date
-  var dateDate  = new Date().toLocaleDateString(undefined, {day:   '2-digit'});
-  var dateMonth = new Date().toLocaleDateString(undefined, {month: 'long'});
-  var date      = dateDate + " " + dateMonth;
-  drawText(ctx, date, 'white', 'center', '14px bold Gothic', cx, cy + 27);
+  var date   = new Date().toLocaleDateString(undefined, {day:   '2-digit'});
+  drawText(ctx, date, 'gray', 'center', '14px bold Gothic', width - 33, cy - 5);
   
   // Define Max Length of Hands
   var maxLength = (Math.min(width, height) - 20) / 2;
