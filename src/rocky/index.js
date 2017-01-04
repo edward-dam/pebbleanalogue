e781b702c0ba7a79e39f67a4ee396a84e79a3e5a
Edward Dam <edward.dam@gmail.com>
2017-01-04 22:08:00 +0000

    Updated to v1.3

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -53,25 +53,25 @@ rocky.on('draw', function(event) {
   var cy = height / 2;
   
   // Draw Numbers
-  drawText(ctx, '1',  'gray',  'center', '28px bold Droid-serif', cx + 40, - 6);
-  drawText(ctx, '2',  'gray',  'right',  '28px bold Droid-serif', width,   cy     - 55);
+  drawText(ctx, '1',  'white', 'center', '28px bold Droid-serif', cx + 40, - 6);
+  drawText(ctx, '2',  'white', 'right',  '28px bold Droid-serif', width,   cy     - 55);
   drawText(ctx, '3',  'white', 'right',  '28px bold Droid-serif', width,   cy     - 18);
-  drawText(ctx, '4',  'gray',  'right',  '28px bold Droid-serif', width,   cy     + 20);
-  drawText(ctx, '5',  'gray',  'center', '28px bold Droid-serif', cx + 40, height - 30);
+  drawText(ctx, '4',  'white', 'right',  '28px bold Droid-serif', width,   cy     + 20);
+  drawText(ctx, '5',  'white', 'center', '28px bold Droid-serif', cx + 40, height - 30);
   drawText(ctx, '6',  'white', 'center', '28px bold Droid-serif', cx,      height - 30);
-  drawText(ctx, '7',  'gray',  'center', '28px bold Droid-serif', cx - 40, height - 30);
-  drawText(ctx, '8',  'gray',  'left',   '28px bold Droid-serif', 0,       cy     + 20);
+  drawText(ctx, '7',  'white', 'center', '28px bold Droid-serif', cx - 40, height - 30);
+  drawText(ctx, '8',  'white', 'left',   '28px bold Droid-serif', 0,       cy     + 20);
   drawText(ctx, '9',  'white', 'left',   '28px bold Droid-serif', 0,       cy     - 18);
-  drawText(ctx, 'T',  'gray',  'left',   '28px bold Droid-serif', 0,       cy     - 55);
-  drawText(ctx, 'E',  'gray',  'center', '28px bold Droid-serif', cx - 40, - 6);
+  drawText(ctx, 'T',  'white', 'left',   '28px bold Droid-serif', 0,       cy     - 55);
+  drawText(ctx, 'E',  'white', 'center', '28px bold Droid-serif', cx - 40, - 6);
   drawText(ctx, '12', 'white', 'center', '28px bold Droid-serif', cx,      - 6);
   
   // Draw Day
   var day = new Date().toLocaleDateString(undefined, {day: 'short'});
-  drawText(ctx, day, 'white', 'center', '14px bold Gothic', width - 33, cy - 15);
+  drawText(ctx, day, 'gray', 'center', '14px bold Gothic', width - 33, cy - 15);
 
   // Draw Date
-  var date   = new Date().toLocaleDateString(undefined, {day:   '2-digit'});
+  var date = new Date().toLocaleDateString(undefined, {day: 'numeric'});
   drawText(ctx, date, 'gray', 'center', '14px bold Gothic', width - 33, cy - 3);
 
   // Define Max Length of Hands
