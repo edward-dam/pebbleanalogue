78642cd8cde222d1393802159e0b79d2498c8fb7
Edward Dam <edward.dam@gmail.com>
2017-01-04 10:41:48 +0000

    Updated to v1.1

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -51,31 +51,24 @@ rocky.on('draw', function(event) {
   // Define Centre of Screen
   var cx = width  / 2;
   var cy = height / 2;
-
-  // Draw Brand
-  //drawText(ctx, 'pebble', 'gray', 'center', '18px bold Gothic', cx, cy + 25);
-
+  
   // Draw Numbers
-  drawText(ctx, '1',  'white', 'center', '28px bold Droid-serif', cx + 40, - 6);
-  drawText(ctx, '2',  'white', 'right',  '28px bold Droid-serif', width,   cy     - 55);
+  drawText(ctx, 'a',  'gray',  'center', '28px bold Droid-serif', cx + 40, - 6);
+  drawText(ctx, 'n',  'gray',  'right',  '28px bold Droid-serif', width,   cy     - 55);
   drawText(ctx, '3',  'white', 'right',  '28px bold Droid-serif', width,   cy     - 18);
-  drawText(ctx, '4',  'white', 'right',  '28px bold Droid-serif', width,   cy     + 20);
-  drawText(ctx, '5',  'white', 'center', '28px bold Droid-serif', cx + 40, height - 29);
-  drawText(ctx, '6',  'white', 'center', '28px bold Droid-serif', cx,      height - 29);
-  drawText(ctx, '7',  'white', 'center', '28px bold Droid-serif', cx - 40, height - 29);
-  drawText(ctx, '8',  'white', 'left',   '28px bold Droid-serif', 0,       cy     + 20);
+  drawText(ctx, 'a',  'gray',  'right',  '28px bold Droid-serif', width,   cy     + 20);
+  drawText(ctx, 'l',  'gray',  'center', '28px bold Droid-serif', cx + 40, height - 30);
+  drawText(ctx, '6',  'white', 'center', '28px bold Droid-serif', cx,      height - 30);
+  drawText(ctx, 'o',  'gray',  'center', '28px bold Droid-serif', cx - 40, height - 30);
+  drawText(ctx, 'g',  'gray',  'left',   '28px bold Droid-serif', 0,       cy     + 20);
   drawText(ctx, '9',  'white', 'left',   '28px bold Droid-serif', 0,       cy     - 18);
-  drawText(ctx, '10', 'white', 'left',   '28px bold Droid-serif', 0,       cy     - 55);
-  drawText(ctx, '11', 'white', 'center', '28px bold Droid-serif', cx - 40, - 6);
+  drawText(ctx, 'u',  'gray',  'left',   '28px bold Droid-serif', 0,       cy     - 55);
+  drawText(ctx, 'e',  'gray',  'center', '28px bold Droid-serif', cx - 40, - 6);
   drawText(ctx, '12', 'white', 'center', '28px bold Droid-serif', cx,      - 6);
-
-  // Draw Name
-  //drawText(ctx, 'Ed',  'gray',  'center', '14px bold Gothic', 35, cy - 15);
-  //drawText(ctx, 'Dam', 'gray',  'center', '14px bold Gothic', 35, cy - 5);
-
+  
   // Draw Day
   var day = new Date().toLocaleDateString(undefined, {day: 'short'});
-  drawText(ctx, day, 'gray', 'center', '14px bold Gothic', width - 33, cy - 15);
+  drawText(ctx, day, 'white', 'center', '14px bold Gothic', width - 33, cy - 15);
 
   // Draw Date
   var date   = new Date().toLocaleDateString(undefined, {day:   '2-digit'});
