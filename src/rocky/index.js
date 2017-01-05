a006d59ab6923343807498e5a50b1b7917ce27f2
Edward Dam <edward.dam@gmail.com>
2017-01-05 12:07:43 +0000

    Updated to v1.5
    Support added for Chalk (Pebble Time Round).

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -41,41 +41,41 @@ rocky.on('minutechange', function(event) {
 rocky.on('draw', function(event) {
   var ctx = event.context;
 
-  // Clear Canvas
-  ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-
   // Define Screen Size
-  var width  = ctx.canvas.unobstructedWidth;
-  var height = ctx.canvas.unobstructedHeight;
+  var mx = ctx.canvas.unobstructedWidth;
+  var my = ctx.canvas.unobstructedHeight;
+  
+  // Clear Canvas
+  ctx.clearRect(0, 0, mx, my);
 
   // Define Centre of Screen
-  var cx = width  / 2;
-  var cy = height / 2;
+  var cx = mx / 2;
+  var cy = my / 2;
   
   // Draw Numbers
-  drawText(ctx, '1',  'white', 'center', '28px bold Droid-serif', cx + 40, - 6);
-  drawText(ctx, '2',  'white', 'right',  '28px bold Droid-serif', width,   cy     - 55);
-  drawText(ctx, '3',  'white', 'right',  '28px bold Droid-serif', width,   cy     - 18);
-  drawText(ctx, '4',  'white', 'right',  '28px bold Droid-serif', width,   cy     + 20);
-  drawText(ctx, '5',  'white', 'center', '28px bold Droid-serif', cx + 40, height - 30);
-  drawText(ctx, '6',  'white', 'center', '28px bold Droid-serif', cx,      height - 30);
-  drawText(ctx, '7',  'white', 'center', '28px bold Droid-serif', cx - 40, height - 30);
-  drawText(ctx, '8',  'white', 'left',   '28px bold Droid-serif', 0,       cy     + 20);
-  drawText(ctx, '9',  'white', 'left',   '28px bold Droid-serif', 0,       cy     - 18);
-  drawText(ctx, 'T',  'white', 'left',   '28px bold Droid-serif', 0,       cy     - 55);
-  drawText(ctx, 'E',  'white', 'center', '28px bold Droid-serif', cx - 40, - 6);
-  drawText(ctx, '12', 'white', 'center', '28px bold Droid-serif', cx,      - 6);
-  
+  drawText(ctx, '1',  'white', 'center', '28px bold Droid-serif', cx + 40, cy - 90);
+  drawText(ctx, '2',  'white', 'right',  '28px bold Droid-serif', cx + 72, cy - 55);
+  drawText(ctx, '3',  'white', 'right',  '28px bold Droid-serif', cx + 72, cy - 18);
+  drawText(ctx, '4',  'white', 'right',  '28px bold Droid-serif', cx + 72, cy + 20);
+  drawText(ctx, '5',  'white', 'center', '28px bold Droid-serif', cx + 40, cy + 54);
+  drawText(ctx, '6',  'white', 'center', '28px bold Droid-serif', cx,      cy + 54);
+  drawText(ctx, '7',  'white', 'center', '28px bold Droid-serif', cx - 40, cy + 54);
+  drawText(ctx, '8',  'white', 'left',   '28px bold Droid-serif', cx - 72, cy + 20);
+  drawText(ctx, '9',  'white', 'left',   '28px bold Droid-serif', cx - 72, cy - 18);
+  drawText(ctx, '0',  'white', 'left',   '28px bold Droid-serif', cx - 72, cy - 55);
+  drawText(ctx, 'E',  'white', 'center', '28px bold Droid-serif', cx - 40, cy - 90);
+  drawText(ctx, '12', 'white', 'center', '28px bold Droid-serif', cx,      cy - 90);
+
   // Draw Day
   var day = new Date().toLocaleDateString(undefined, {day: 'short'});
-  drawText(ctx, day, 'gray', 'center', '14px bold Gothic', width - 33, cy - 15);
-
+  drawText(ctx, day, 'gray', 'center', '14px bold Gothic', cx + 39, cy - 15);
+  
   // Draw Date
   var date = new Date().toLocaleDateString(undefined, {day: 'numeric'});
-  drawText(ctx, date, 'gray', 'center', '14px bold Gothic', width - 33, cy - 3);
-
+  drawText(ctx, date, 'gray', 'center', '14px bold Gothic', cx + 39, cy - 3);
+  
   // Define Max Length of Hands
-  var maxLength = (Math.min(width, height) - 15) / 2;
+  var maxLength = (Math.min(mx, my) - 15) / 2;
 
   // Draw Minute Hand
   var minuteFraction = (new Date().getMinutes()) / 60;
